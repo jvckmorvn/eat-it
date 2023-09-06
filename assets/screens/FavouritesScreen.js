c5aac9b7e4534694996a603c90c079c6f60a7c8a
Jack Moran <jvckmorvn@gmail.com>
2023-09-06 21:35:22 +0100

    favourites screen

--- assets/screens/FavouritesScreen.js
+++ assets/screens/FavouritesScreen.js
@@ -1,15 +1,38 @@
-import { useContext } from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import { useContext } from 'react';
 
-import { FavouritesContext } from "../store/context/favourites-context";
-import MealsList from "../components/MealsList/MealsList";
-import { MEALS } from "../data/dummy-data";
+import { FavouritesContext } from '../store/context/favourites-context';
+import MealsList from '../components/MealsList/MealsList';
+import { MEALS } from '../data/dummy-data';
 
 export default function FavouritesScreen() {
   const favouriteMealsCtx = useContext(FavouritesContext);
 
   const favouriteMeals = MEALS.filter(meal => favouriteMealsCtx.ids.includes(meal.id))
 
+  if (favouriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favourite meals yet</Text>
+      </View>
+    )
+  }
+
   return (
     <MealsList items={favouriteMeals}/>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 160
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'brown'
+  }
+});
